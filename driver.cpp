1b565e605c809d55fc58cc1a2d6261a907d6829a
Roger Hsiao <rogerhh@umich.edu>
2022-05-08 00:30:17 +0000

    change driver

--- driver.cpp
+++ driver.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-    for(int num_cores = 0; num_cores < 32; num_cores += 2) {
+    for(int num_cores = 1; num_cores < 2; num_cores += 2) {
         int opt = -1;
         int omp_num_threads = num_cores;
 
@@ -28,10 +28,11 @@ int main(int argc, char** argv) {
 
         SchurOpt schur_opt(omp_num_threads);
 
-        string list_fname = "filelist_sample.csv";
+        string list_fname = "../filelist.csv";
         ifstream list_fin(list_fname);
         if(!list_fin.is_open()) {
             cerr << "Error opening file: " << list_fname << endl;
+            exit(1);
         }
 
         string A_fname, B_fname, D_fname, b_fname, Dschur_fname, bschur_fname;
